76338495f722c5000d0f697ebe7e9ff3c45dfb80
Cindy-O <cindy.orbegoso@yahoo.ca>
2017-02-14 03:23:27 -0500

    Uploading game onto  GitHub

---
+++ NumGuessingGame/GameForm.Designer.vb
@@ -0,0 +1,104 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmGame
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmGame))
+        Me.lblInfo = New System.Windows.Forms.Label()
+        Me.txtGuess = New System.Windows.Forms.TextBox()
+        Me.lblInfo2 = New System.Windows.Forms.Label()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.lblResults = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblInfo
+        '
+        Me.lblInfo.AutoSize = True
+        Me.lblInfo.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInfo.Location = New System.Drawing.Point(20, 9)
+        Me.lblInfo.Name = "lblInfo"
+        Me.lblInfo.Size = New System.Drawing.Size(244, 16)
+        Me.lblInfo.TabIndex = 0
+        Me.lblInfo.Text = "Try to guess the number I am thinking of!"
+        Me.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtGuess
+        '
+        Me.txtGuess.Location = New System.Drawing.Point(46, 69)
+        Me.txtGuess.Name = "txtGuess"
+        Me.txtGuess.Size = New System.Drawing.Size(70, 20)
+        Me.txtGuess.TabIndex = 1
+        Me.txtGuess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lblInfo2
+        '
+        Me.lblInfo2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInfo2.Location = New System.Drawing.Point(0, 29)
+        Me.lblInfo2.Name = "lblInfo2"
+        Me.lblInfo2.Size = New System.Drawing.Size(286, 36)
+        Me.lblInfo2.TabIndex = 2
+        Me.lblInfo2.Text = "Please enter a guess between 1-100 and submit your guess by pressing the button o" & _
+    "r pressing the ""enter"" key."
+        Me.lblInfo2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(166, 68)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 3
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'lblResults
+        '
+        Me.lblResults.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResults.Location = New System.Drawing.Point(0, 100)
+        Me.lblResults.Name = "lblResults"
+        Me.lblResults.Size = New System.Drawing.Size(286, 36)
+        Me.lblResults.TabIndex = 4
+        Me.lblResults.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'frmGame
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 145)
+        Me.Controls.Add(Me.lblResults)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.lblInfo2)
+        Me.Controls.Add(Me.txtGuess)
+        Me.Controls.Add(Me.lblInfo)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmGame"
+        Me.Text = "Cindy's NumGuess Game!"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblInfo As System.Windows.Forms.Label
+    Friend WithEvents txtGuess As System.Windows.Forms.TextBox
+    Friend WithEvents lblInfo2 As System.Windows.Forms.Label
+    Friend WithEvents btnSubmit As System.Windows.Forms.Button
+    Friend WithEvents lblResults As System.Windows.Forms.Label
+
+End Class
---
+++ NumGuessingGame/GameForm.vb
@@ -0,0 +1,69 @@
+﻿'Cindy's NumGuess Game
+'author: Cindy Orbegoso
+'date: December 20, 2015
+'The user enters a number guess and the game gives hints until the user
+'guesses the number correctly.
+
+Public Class frmGame
+    'variables to store the number answer and the user's guess
+    Dim answer As Integer
+    Dim guess As Integer
+
+    Private Sub frmGame_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            CheckWin()
+        End If
+    End Sub
+    Private Sub frmGame_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        startGame()
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        CheckWin()
+    End Sub
+
+    Private Sub startGame()
+        Randomize()
+        answer = CInt(Int((100 * Rnd()) + 1))
+        lblResults.Text = ""
+    End Sub
+
+    Private Sub CheckWin()
+        'check if the user's guess is a number
+        If IsNumeric(txtGuess.Text) Then
+            'convert user's String guess to an integer
+            guess = CInt(txtGuess.Text)
+            'check if user's guess is invalid, too low, too high, or correct
+            Select Case guess
+                Case Is < 1
+                    lblResults.ForeColor = Color.Red
+                    lblResults.Text = "Your guess is invalid."
+                Case Is > 100
+                    lblResults.ForeColor = Color.Red
+                    lblResults.Text = "Your guess is invalid."
+                Case Is < answer
+                    lblResults.ForeColor = Color.Blue
+                    lblResults.Text = "Your guess is too low!"
+                Case Is > answer
+                    lblResults.ForeColor = Color.Blue
+                    lblResults.Text = "Your guess is too high!"
+                Case Else
+                    Dim response = MsgBox("You guessed correctly!" & vbCrLf & "Would you like to play again?", MsgBoxStyle.YesNo, "Hooray!")
+                    If response = MsgBoxResult.Yes Then
+                        'Start game over
+                        startGame()
+                    ElseIf response = MsgBoxResult.No Then
+                        'close game
+                        Me.Close()
+                    End If
+            End Select
+        End If
+        txtGuess.Text = ""
+    End Sub
+
+    Private Overloads Sub txtGuess_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles txtGuess.KeyDown
+        If e.KeyCode = Keys.Return Or e.KeyCode = Keys.Enter Then
+            CheckWin()
+        End If
+    End Sub
+End Class
